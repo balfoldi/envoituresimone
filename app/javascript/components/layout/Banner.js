17a2fa0a651d243eb34c918fbe6c420f54d9a1bc
BorisSIUS <balfooldi@yahoo.fr>
2021-11-11 13:42:02 +0100

    Create application

---
+++ app/javascript/components/layout/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../../stylesheets/banner"
+
+function Banner() {
+  return (
+    <div class="fat-hero-image d-flex shadow justify-content-around py-5 mb-3" >
+      <div class="d-flex flex-wrap text-white justify-content-around">
+        <h1>En Voiture Simone !</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
