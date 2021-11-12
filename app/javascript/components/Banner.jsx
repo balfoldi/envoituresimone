8b638f805026feef7a8c3a98000c29c8443809fe
BorisSIUS <balfooldi@yahoo.fr>
2021-11-12 21:45:02 +0100

    Beautify home page

--- app/javascript/components/Banner.jsx
+++ app/javascript/components/Banner.jsx
@@ -3,11 +3,10 @@ import "../stylesheets/banner"
 
 function Banner() {
   return (
-    <div className="fat-hero-image d-flex flex-column justify-content-between  py-2 mb-3 px-2" >
-      <div className="d-flex justify-content-around">
+    <div className="fat-hero-image d-flex flex-column justify-content-between py-2px-2" >
+      <div className="text-light d-flex justify-content-around">
         <h1>En Voiture Simone !</h1>
       </div>
-      <i className="text-light d-flex justify-content-end mr-3">Illustration par Marie   </i>
     </div>
   )
 }
